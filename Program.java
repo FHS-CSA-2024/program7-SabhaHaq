57aedffa043e8017e403ab9bd527d142a80c90a2
SabhaHaq <s.h.haq826@gmail.com>
2024-09-02 22:06:54 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,9 +1,16 @@
-//import stuff here
 
-//Your code here
+public class Program7 {
 
-//Paste console output below:
-/*
+       private static final int klevinPerShruteBuck= 20; 
+       private static final int stanleyNickelsPerKlevin= 12; 
+       private static final int stanleyNickelPerShruteBuck = klevinPerShruteBuck*stanleyNickelsPerKlevin; 
+   
+ public static double convert ( double shruteBucks, double klevins, double stanleyNickels) {
+          double totalStanleyNickels = (shruteBucks*stanleyNickelPerShruteBuck)+
+          (klevins* stanleyNickelsPerKlevin) + stanleyNickels;
+          double decimalShruteBucks= totalStanleyNickels / stanleyNickelPerShruteBuck; 
+          return decimalShruteBucks;
+        }
+          
 
-
-*/
+        }
